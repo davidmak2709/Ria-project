eeb835233eb857c45d3816ee98ba677a1ce78680
david <davidmakovac95@gmail.com>
2018-01-10 23:30:29 +0100

    Dodavanje slika i galerija

--- app/controllers/DetailsController.php
+++ app/controllers/DetailsController.php
@@ -12,10 +12,16 @@ class DetailsController extends Controller{
 	public function indexAction(){
 		$id = $_GET["id"];
 
+
+
 		$this->view->bar = Klub::findFirst($id);
 
 		$this->view->bar->follow = Pretplata::isFollowed($this->session->get("id"),$id);
 
+		$this->view->images = scandir("img/" . $this->view->bar->id_Klub);
+		$this->view->images = $files = array_diff($this->view->images, array('.', '..'));
+
+		
 		if($id == null || $this->view->bar == null) 
 			$this->response->redirect("index");
 	}
